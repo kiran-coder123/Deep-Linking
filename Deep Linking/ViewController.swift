9f97045fae4d0cf846e7a280956e0e612fb466f1
kiran <kiran.sonne@mobisoftinfotech.com>
2021-09-12 15:02:21 +0530

    Initial Commit

---
+++ Deep Linking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Deep Linking
+//
+//  Created by Kiran Sonne on 12/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
