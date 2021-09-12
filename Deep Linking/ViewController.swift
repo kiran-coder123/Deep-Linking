699732f82e104aaa27a3028e6bac968c5422e77a
kiran <kiran.sonne@mobisoftinfotech.com>
2021-09-12 15:11:49 +0530

    Learned how to send data from one app to another.

--- Deep Linking/ViewController.swift
+++ Deep Linking/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passDataTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func deepLinkingAction(_ sender: Any) {
+        let message = passDataTextField.text?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let application = UIApplication.shared
+        let secondAppPath = "secondaryAppDL://secondPage?message=\(message ?? "")"
+        let appUrl = URL(string: secondAppPath)!
+        let websiteUrl = URL(string: "https://www.google.com")!
+        
+        if application.canOpenURL(appUrl) {
+            application.open(appUrl, options: [:], completionHandler: nil)
+            
+        }else {
+            application.open(websiteUrl)
+        }
+        
+    }
+    
 }
-
+ 
